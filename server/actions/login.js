0b76874e7dbd3f315a934401bb5ef6d8e8012aeb
Karpo Korõtko <karpo.korotko@playtech.com>
2019-11-30 19:27:28 +0200

    .

--- server/actions/login.js
+++ server/actions/login.js
@@ -28,30 +28,33 @@ export default async function loginPage(page, counter) {
         await page.waitForXPath(SEL_USERNAME).then((result) => result.click());
         await page.keyboard.type(CREDS.username);
         await page.waitForXPath(SEL_PASSWORD).then((result) => result.click());
-        await page.keyboard.type(CREDS.password);
+            await page.keyboard.type(CREDS.password);
         console.log("Username/Password entered");
         
         await Promise.all([page.keyboard.press('Enter'), 
             page.waitForNavigation({ waitUntil: 'networkidle0' }).then(async () => {
-            //Check page title of the loaded page
-            await page.title().then(async result => {
-                console.log("Page finished loading. Title: " + result);
-                // Check if logged in page is opened
-                if (result === LOGIN_TITLE) {
-                    // End login flow and return the resulting page object
-                    console.log("Fantrax user logged in");
-                } else {
-                    // Throw error about incorrect page title
-                    throw new Error('Incorrect login page');
-                }
-            });
-        }, async () => {
-            // If wait for navigation fails, check if login error is present
-            await page.waitForXPath(SEL_LOGIN_ERROR).then(async (result) => { 
-                console.log("Login failed: Invalid username/password.");
-                throw new Error('Login failed') 
-            });
-        })]);
+                //Check page title of the loaded page
+                await page.title().then(async result => {
+                    console.log("Page finished loading. Title: " + result);
+                    // Check if logged in page is opened
+                    if (result === LOGIN_TITLE) {
+                        // End login flow and return the resulting page object
+                        console.log("Fantrax user logged in");
+                    } else {
+                        // Throw error about incorrect page title
+                        throw new Error('Incorrect login page');
+                    }
+                });
+            }, async (error) => {
+                // If wait for navigation fails, check if login error is present
+                console.log("Waiting for response for login page failed");
+                console.log(error);
+                await page.waitForXPath(SEL_LOGIN_ERROR).then(async (result) => { 
+                    console.log("Login failed: Invalid username/password.");
+                    throw new Error('Login failed') 
+                });
+            })
+        ]);
     } catch (e) {
         console.log("Error logging in: " + e);
         // Close the browser and increase counter of tries
